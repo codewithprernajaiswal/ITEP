f3c4f23e9267086b09c0ccd7c303e2e4267e4d55
codewithprernajaiswal <prernajaiswal143@gmail.com>
2025-07-17 17:23:40 +0530

    Delete infojava/3rdAssingment directory
    delete assignment3 file

--- infojava/3rdAssingment/q272.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-class Assignment3{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter a character  =");
-         String input = sc.nextLine(); 
-        char ch=input.charAt(0);
-        switch()
